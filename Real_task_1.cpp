d048a40db6f713b52f709ab7985b8a0ee21b6535
@Maxim_Kiryakin <93873319+MaximKiryakin@users.noreply.github.com>
2022-10-03 20:46:25 +0300

    Add files via upload

--- Real_task_1.cpp
+++ Real_task_1.cpp
@@ -3,12 +3,13 @@
 #include <fstream>
 #include <cmath>
 #include <ctime> 
+#include <chrono>
 
 // Функиция выполняет перемножение матриц PAP
 int pap_transformation(std::vector<double>& a, std::vector<double>& b,
-    std::vector<std::vector<double>>& matrix, double digit)
+    std::vector<double> &matrix, double digit)
 {
-    if (a.size() != b.size() || a.size() != matrix.size())
+    if (a.size() != b.size() || a.size() != sqrt(matrix.size()))
         return 1;
 
     int size = a.size();
@@ -17,8 +18,8 @@ int pap_transformation(std::vector<double>& a, std::vector<double>& b,
     {
         for (int j = 0; j < size; j++)
         {
-            matrix[i][j] -= a[i] * b[j] * digit;
-            matrix[i][j] -= a[j] * b[i] * digit;
+            matrix[i*size + j] -= a[i] * b[j] * digit;
+            matrix[i*size + j] -= a[j] * b[i] * digit;
         }
     }
     return 0;
@@ -26,16 +27,16 @@ int pap_transformation(std::vector<double>& a, std::vector<double>& b,
 
 
 // печать матрицы в файл 
-void print_matrix_to_file(std::vector<std::vector<double>> matrix, std::string name)
+void print_matrix_to_file(std::vector<double> matrix, std::string name)
 {
-    int size = matrix.size();
+    int size = sqrt(matrix.size());
     std::ofstream fout(name);
 
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
         {
-            fout << matrix[j][i] << " ";
+            fout << matrix[j * size + i] << " ";
         }
         fout << std::endl;
     }
@@ -44,16 +45,19 @@ void print_matrix_to_file(std::vector<std::vector<double>> matrix, std::string n
 
 
 // создание пустой матрицы
-std::vector<std::vector<double>> create_matrix(int size)
+//std::vector<std::vector<double>> create_matrix(int size)
+std::vector<double> create_matrix(int size)
 {
-    std::vector<std::vector<double>> tmp(size);
+    //std::vector<std::vector<double>> tmp(size);
 
-    for (int i = 0; i < size; i++)
-    {
-        std::vector<double> a(size);
-        tmp[i] = a;
-    }
-    return tmp;
+    //for (int i = 0; i < size; i++)
+    //{
+    //    std::vector<double> a(size);
+    //    tmp[i] = a;
+    //}
+    //return tmp;
+    std::vector<double> a(size * size);
+    return a;
 }
 
 // скалярное умножение векторов
@@ -71,10 +75,10 @@ int dot(std::vector<double>& a, std::vector<double>& b, double& ans)
 }
 
 // умножение матрицы на вектор
-int dot_matrix_vector(std::vector<std::vector<double>>& matrix, std::vector<double>& b, std::vector<double>& ans)
+int dot_matrix_vector(std::vector<double>& matrix, std::vector<double>& b, std::vector<double>& ans)
 {
-    if (matrix.size() != b.size())
-        return 1;
+    //if (matrix.size() != b.size())
+    //    return 1;
 
     int size = ans.size();
 
@@ -82,37 +86,37 @@ int dot_matrix_vector(std::vector<std::vector<double>>& matrix, std::vector<doub
     {
         ans[k] = 0;
         for (int i = 0; i < size; i++)
-            ans[k] += matrix[k][i] * b[i];
+            ans[k] += matrix[i + k*size] * b[i];
     }
     return 0;
 }
 
 
 // преобразование матрицы по алгоритму Хаусхолдера
-int hausholders_transformation(std::vector<std::vector<double>>& matrix)
+int hausholders_transformation(std::vector<double> &matrix)
 {
-    int size = matrix.size();
+    int size = sqrt(matrix.size());
 
     for (int i = 0; i < size - 2; i++)
     {
         double norm = 0;
         for (int j = i + 1; j < size; j++)
         {
-            norm += matrix[j][i] * matrix[j][i];
+            norm += matrix[size*j + i] * matrix[size*j + i];
         }
         norm = sqrt(norm);
 
-        double s = std::copysignf(1, matrix[i + 1][i]) * norm;
-        double r = sqrt(2 * matrix[i + 1][i] * s + 2 * s * s);
+        double s = std::copysignf(1, matrix[(i + 1)*size + i]) * norm;
+        double r = sqrt(2 * matrix[(i + 1)*size + i] * s + 2 * s * s);
 
         std::vector<double> w(size);
-        w[i + 1] = (matrix[i + 1][i] + s) * (1 / r);
+        w[i + 1] = (matrix[(i + 1)*size + i] + s) * (1 / r);
 
         for (int j = i + 2; j < size; j++)
         {
             if (j >= i + 2)
             {
-                w[j] = matrix[j][i] * (1 / r);
+                w[j] = matrix[j*size + i] * (1 / r);
             }
             if (j <= i)
             {
@@ -146,10 +150,10 @@ int hausholders_transformation(std::vector<std::vector<double>>& matrix)
 }
 
 // чтение матрицы из файла
-int read_matrix(std::vector<std::vector<double>>& v, std::string str)
+int read_matrix(std::vector<double>& v, std::string str)
 {
 
-    int size = v.size();
+    int size = sqrt(v.size());
     std::ifstream file(str);
 
     if (!file.is_open())
@@ -160,7 +164,7 @@ int read_matrix(std::vector<std::vector<double>>& v, std::string str)
     {
         for (int j = 0; j < size; j++)
         {
-            file >> v[i][j];
+            file >> v[i*size + j];
         }
     }
 
@@ -169,24 +173,24 @@ int read_matrix(std::vector<std::vector<double>>& v, std::string str)
 
 
 // округление всех элементов матрицы
-void matrix_round(std::vector<std::vector<double>>& v)
+void matrix_round(std::vector<double>& v)
 {
-    int size = v.size();
+    int size = sqrt(v.size());
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
         {
-            double sign = std::copysignf(1, v[i][j]);
-            v[i][j] = sign * round(std::copysignf(1, v[i][j]) * v[i][j] * 100) / 100;
+            double sign = std::copysignf(1, v[i*size +j]);
+            v[i * size + j] = sign * round(std::copysignf(1, v[i * size + j]) * v[i * size + j] * 100) / 100;
         }
     }
 }
 
 int main()
 {
-    int matrix_size = 4096;
-    std::string input_file_name = "matrix4096.txt";
-    std::string output_file_name = "tridiag_matrix4096.txt";
+    int matrix_size = 256;
+    std::string input_file_name = "matrix256.txt";
+    std::string output_file_name = "tridiag_matrix256.txt";
 
     auto matrix = create_matrix(matrix_size);
     if (!read_matrix(matrix, input_file_name))
@@ -194,17 +198,17 @@ int main()
         std::cout << "Err open file";
         return 1;
     }
-    unsigned int start_time = clock();
+    auto start = std::chrono::steady_clock::now();
 
     hausholders_transformation(matrix);
 
-    unsigned int end_time = clock();
-    unsigned int search_time = end_time - start_time;
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
 
     matrix_round(matrix);
     print_matrix_to_file(matrix, output_file_name);
 
-    std::cout << search_time / 1000 << std::endl;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 
     return 0;
 }
